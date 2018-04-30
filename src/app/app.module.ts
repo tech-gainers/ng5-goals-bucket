47fcae2ecd18943753202acfb3a1eb1375f4898a
Tech Gainers <er.cpsharma@gmail.com>
2018-04-30 11:16:26 -0700

    added service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
+import {DataService} from './data.service';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -21,7 +22,7 @@ import { AboutComponent } from './about/about.component';
     FormsModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DataService], /**Add service in providers */
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 /**we need to import animations classes here as well*/
 import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations'
-
+import {DataService} from '../data.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -37,21 +37,26 @@ export class HomeComponent implements OnInit {
   btnText: string ='Add an Item'; /*property to show property binding */
   goalText: string='My first goal';
   goals=[];/* Array to hold the goals*/
-  constructor() { }
+  
+  constructor(private _data:DataService) { }
 
   /**This is a LifeCycle Hook that is called after data-bound properties of a directive is called */
   ngOnInit() {
     this.itemCount=this.goals.length;
+    this._data.goal.subscribe(res => this.goals=res);
+    this._data.changeGoal(this.goals);
   }
 
   addItem(){
     this.goals.push(this.goalText); /**adding to goals array */
     this.goalText='';/**resetting the goal text */
     this.itemCount=this.goals.length;/**setting the item count for update */
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i){
     this.goals.splice(i, 1); /**Removing item */
     this.itemCount=this.goals.length;/**Updating the item count to show on UI */
+    this._data.changeGoal(this.goals);
   }
 }
